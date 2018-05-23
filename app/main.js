ded7c45c9ad7f04d6a91e4a54406cd36aba0df2d
Jozsef Vass <jozsef@discordapp.com>
2018-05-23 08:39:29 -0700

    minor fixes

--- app/main.js
+++ app/main.js
@@ -21,7 +21,7 @@ window.onload = function() {
   width = meter.scrollWidth;
   height = meter.scrollHeight;
   canvasContext = meter.getContext('2d');
-  canvasContext.fillStyle = "green";
+  canvasContext.fillStyle = 'green';
   canvasContext.rect(0, 0, width, height);
   canvasContext.stroke();
 }
@@ -43,13 +43,13 @@ function enumerateDevices() {
     return;
   }
   navigator.mediaDevices.enumerateDevices().then(devices => {
-   const audioInputDevies = document.getElementById("audioInputDevices");
+   const audioInputDevies = document.getElementById('audioInputDevices');
    const index = audioInputDevies.selectedIndex;
    for (let i = audioInputDevies.options.length - 1 ; i >= 0 ; i--) {
       audioInputDevies.remove(i);
     }
     devices.forEach(device => {
-     if (device.kind === "audioinput") {
+     if (device.kind === 'audioinput') {
         const option = document.createElement('option');
         option.text = device.label || device.deviceId;
         option.value = device.deviceId;
@@ -58,7 +58,7 @@ function enumerateDevices() {
     });
     audioInputDevies.selectedIndex = Math.max(0, Math.min(index, audioInputDevies.options.length - 1));
   }).catch(error => {
-    status(`Error getting devices: ${error}\n`);
+    status(`Error getting devices: ${error.toString()}\n`);
   });
 }
 
@@ -69,7 +69,7 @@ window.start = function() {
     stream.getTracks().forEach(track => track.stop());
   }
 
-  const audioInputDevies = document.getElementById("audioInputDevices");
+  const audioInputDevies = document.getElementById('audioInputDevices');
   const selected = audioInputDevies.value;
 
   status(`Using device: ${selected}\n`);
@@ -94,10 +94,6 @@ window.start = function() {
       canvasContext.stroke();
     };
   }).catch(error => {
-    if (error.name == 'PermissionDeniedError') {
-    status(`getUserMedia permission error: ${error.message}\n`);
-    } else {
-      status(`getUserMedia error: ${error}\n`);
-    }
+    status(`getUserMedia error: ${error.toString()}\n`);
   });
 }
